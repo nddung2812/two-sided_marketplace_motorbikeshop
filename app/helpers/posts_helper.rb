1bcc31dcfe5e3dfaf75e697a2d71bcd903e60f12
Dung Nguyen <nddung2812@gmail.com>
2021-08-04 14:36:29 +1000

    add styling for buy button

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
     def render_buy_button(current_user, post)
         if current_user
-          button = "<button data-stripe=\"payment\" class='btn btn-primary p-3'> Buy: $<%=#{@post.price}%></button>"
+          button = "<button data-stripe=\"payment\" class='btn btn-primary p-3'> Buy: $#{@post.price}</button>"
         else
           button = "<button class=\"btn btn-primary\" disabled>Log In to Buy</button>"
         end
